00270723bc0642d712598515eb7d0fe4da612d47
이정배 <jblee@bankbe.net>
2023-11-06 15:23:44 +0900

    Add: 초기 설정

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Accept']
+}));
+
+
+app.post('/requestLogin', (req, res) => {
+  const clientData = req.body;
+  console.log('Received data:', clientData);
+
+  // 응답 데이터
+  const responseData = {
+    user: "홍길동"
+  };
+
+  res.json(responseData);
+});
+
+app.listen(4009, () => {
+  console.log('Server is running on port 4009');
+});
